ca736ed0ba4f4a5669aaa85438915a1b1f20dfb6
dunesfolk <inquire-git@dunesfolk.work>
2025-09-09 17:18:13 +0900

    fix error

--- robocopy-specific-folders.ps1
+++ robocopy-specific-folders.ps1
@@ -1,12 +1,12 @@
 
 $textfile = "" #path to text file with a folder names
-$dirlist | Set-Content -Path $textfile -Encoding UTF8
+$dirlist = Get-Content -Path $textfile
 $src_basepath = ""
 $dst_basepath = ""
 
 foreach ($dir in $dirlist) {
     $datetime = Get-Date -Format "yyyyMMdd_HHmmss"
-    $cp_src = "${src_basepath}\${$dir}\"
+    $cp_src = "${src_basepath}\${dir}\"
     $cp_dst = "${dst_basepath}\${dir}\"
     $cp_log = "${dst_basepath}\_log\$datetime" + "_$dir.log"
     $cp_opt = "/MIR /mt:2 /r:2 /w:2 /np /sl /xf desktop.ini thumbs.db /Log:$cp_log"
